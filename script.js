b8e931713680583dade77784126c3f227be13900
Olga-Vretto <olgavretto@outlook.com>
2023-03-29 20:53:22 +0300

    html/css

---
+++ script.js
@@ -0,0 +1,9 @@
+const getElement = (selector) => {
+  return document.querySelector(selector);
+};
+
+const createElement = (element) => {
+  return document.createElement(element);
+};
+
+const addForm = getElement("#addForm");
